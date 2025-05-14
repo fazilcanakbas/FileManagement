8060088d3725cdb6ed5544e6cbab1e298885ce1c
Fazıl Can Akbaş <fazilcanakbas5@gmail.com>
2025-05-14 22:05:24 +0300

    add

---
+++ FileManagementAPI/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+        protected IActionResult HandleError(Exception ex)
+        {
+<<<<<<< HEAD
+=======
+            
+>>>>>>> f9754c3285c70acc415d849ae93e69ef0cba8fe5
+            Console.WriteLine($"An error occurred: {ex.Message}");
+
+            return StatusCode(500, new
+            {
+                error = "An error occurred while processing your request.",
+                message = ex.Message
+            });
+        }
+    }
+}
